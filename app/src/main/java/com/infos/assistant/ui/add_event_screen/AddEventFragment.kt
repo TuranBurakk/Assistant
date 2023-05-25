0998dc5d34d0c2ff624caf936a82da253bb8d782
burakTuran <buraktt00@gmail.com>
2023-05-25 16:23:33 +0300

    [AddEventScreen][Created logic]

--- app/src/main/java/com/infos/assistant/ui/add_event_screen/AddEventFragment.kt
+++ app/src/main/java/com/infos/assistant/ui/add_event_screen/AddEventFragment.kt
@@ -2,11 +2,15 @@ package com.infos.assistant.ui.add_event_screen
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.infos.assistant.data.TodoData
 import com.infos.assistant.databinding.FragmentAddEventBinding
 import com.infos.assistant.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class AddEventFragment : BaseFragment<FragmentAddEventBinding>(FragmentAddEventBinding::inflate) {
@@ -23,15 +27,25 @@ class AddEventFragment : BaseFragment<FragmentAddEventBinding>(FragmentAddEventB
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.dateTv.text = args.date
+        val stringDate = args.date
+        val format = SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale("tr", "TR"))
+        val date: Date = format.parse(stringDate) as Date
+        binding.dateTv.text = date.toString()
         binding.addEventBt.setOnClickListener {
             if (!binding.eventTitleEt.text.isNullOrEmpty()){
+
                 if (!binding.eventDescEt.text.isNullOrEmpty()){
+
                     val title = binding.eventTitleEt.text.toString()
                     val desc = binding.eventDescEt.text.toString()
-                    val date = args.date.toLong()
-                }
-            }
+
+                    val event = TodoData(title = title, explanation = desc, date = date)
+
+                    viewModel.addEvent(event,requireContext())
+
+                }else Toast.makeText(requireContext(),"Description cannot be empty",Toast.LENGTH_LONG).show()
+
+            }else Toast.makeText(requireContext(),"Title cannot be empty",Toast.LENGTH_LONG).show()
         }
 
     }
