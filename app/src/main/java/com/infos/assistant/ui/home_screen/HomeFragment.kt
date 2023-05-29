bf377112996dd57a02abe2c6a1a88dda2dad5579
burakTuran <buraktt00@gmail.com>
2023-05-29 19:24:13 +0300

    [HomeScreen][Created design]

--- app/src/main/java/com/infos/assistant/ui/home_screen/HomeFragment.kt
+++ app/src/main/java/com/infos/assistant/ui/home_screen/HomeFragment.kt
@@ -1,15 +1,26 @@
-package com.infos.assistant
+package com.infos.assistant.ui.home_screen
 
 import android.os.Bundle
 import android.view.View
-import com.infos.assistant.base.BaseFragment
 import com.infos.assistant.databinding.FragmentHomeBinding
+import com.infos.assistant.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+
+    override fun onStart() {
+        super.onStart()
+        showToolBar()
+        showBottomBar()
+        hideTextview()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
+
+
 }
