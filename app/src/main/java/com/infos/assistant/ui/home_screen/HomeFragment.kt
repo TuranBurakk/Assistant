602ca80efaff571705750fd879ee9fa3bafa3a8b
burakTuran <buraktt00@gmail.com>
2023-06-06 11:12:46 +0300

    [HomeScreen][Connected api ]

--- app/src/main/java/com/infos/assistant/ui/home_screen/HomeFragment.kt
+++ app/src/main/java/com/infos/assistant/ui/home_screen/HomeFragment.kt
@@ -2,6 +2,9 @@ package com.infos.assistant.ui.home_screen
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.infos.assistant.data.remote.RequestMessage
 import com.infos.assistant.databinding.FragmentHomeBinding
 import com.infos.assistant.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,6 +12,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
+    private val viewModel: HomeViewModel by viewModels()
+    private val list = mutableListOf<RequestMessage>()
 
     override fun onStart() {
         super.onStart()
@@ -20,7 +25,20 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.sendBtn.setOnClickListener {
+            if (!binding.messageEditText.text.isNullOrEmpty()) {
+
+                val request = binding.messageEditText.text.toString()
+                val role = "user"
+                val message = RequestMessage(role,request)
+                list.add(message)
+                viewModel.chat(list)
+
+            } else Toast.makeText(requireContext(),"Message can not be empty",Toast.LENGTH_LONG).show()
+
+
+        }
+
+
     }
-
-
 }
--- app/src/main/java/com/infos/assistant/ui/home_screen/HomeViewModel.kt
+++ app/src/main/java/com/infos/assistant/ui/home_screen/HomeViewModel.kt
@@ -1,11 +1,16 @@
 package com.infos.assistant.ui.home_screen
 
 import androidx.lifecycle.ViewModel
+import com.infos.assistant.data.remote.RequestMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val useCase: HomeUseCase
+) : ViewModel() {
 
-
+    fun chat(message:List<RequestMessage>){
+        useCase.invoke(message)
+    }
 }
